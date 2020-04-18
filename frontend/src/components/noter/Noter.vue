6cff38f50ef58ccca49522f4533a52ee63ab2df6
Simen Røkaas <simen@simenro.net>
2020-04-18 23:02:53 +0200

    Lagt til primevue og mock-service.

---
+++ frontend/src/components/noter/Noter.vue
@@ -0,0 +1,36 @@
+<template>
+  <DataTable :value="noter">
+    <Column field="id" header="ID"></Column>
+    <Column field="year" header="Year"></Column>
+    <Column field="tittel" header="Tittel"></Column>
+    <Column field="color" header="Color"></Column>
+  </DataTable>
+</template>
+
+<script>
+import NoteService from '../../service/NoteService';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+export default {
+  name: "Noter",
+  data() {
+    return {
+      noter: null
+    }
+  },
+  noteService: null,
+  created() {
+    this.noteService = new NoteService();
+  },
+  mounted() {
+    //this.noteService.fetchNoter().then(data => this.noter = data);
+    this.noter = this.noteService.fetchNoter()
+  },
+  components: {
+    DataTable,
+    Column
+  }
+}
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Noter from '@/components/noter/Noter';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'noter',
+      component: Noter
+    }
+  ]
+});
