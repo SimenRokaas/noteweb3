67485b4c2be5ff9012eda5468149a38d8046f9f6
Simen Røkaas <simen@simenro.net>
2020-08-23 21:50:05 +0200

    Implementert endring av note.

--- routes/noter.js
+++ routes/noter.js
@@ -16,4 +16,29 @@ router.get("/list", (req, res) => {
   });
 });
 
+router.put("/:id", (req, res) => {
+  var arkivNr = req.params.id;
+  var form_data = {
+    tittel1: req.body.tittel1,
+    tittel2: req.body.tittel2,
+    soloInstrument: req.body.soloInstrument,
+    durata: req.body.durata,
+    kategori1: req.body.kategori1,
+    kategori2: req.body.kategori2,
+    kategori3: req.body.kategori3,
+    kommentar: req.body.kommentar
+  };
+  pool.query(
+    "update noter set ? where arkivNr = ?",
+    [form_data, arkivNr],
+    function(error, results) {
+      if (error) {
+        console.error(JSON.stringify(error));
+        throw error;
+      }
+      res.send(JSON.stringify(results));
+    }
+  );
+});
+
 module.exports = router;
