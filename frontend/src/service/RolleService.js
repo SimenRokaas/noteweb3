047c95d22b400be1fc27933529f4ee842dfa0854
Simen Røkaas <simen@simenro.net>
2020-09-01 21:33:54 +0200

    +Pålogging.

--- frontend/src/service/RolleService.js
+++
@@ -1,9 +0,0 @@
-import Api from "@/service/Api";
-
-export default class RolleService {
-  static fetchRolle() {
-    return Api()
-      .get("rolle/")
-      .then(res => res.data.kanSkrive);
-  }
-}
