ae2d8ea9177084ccd3fa014575489038995b1125
Simen Røkaas <simen@simenro.net>
2020-09-05 23:27:55 +0200

    +Autocomplete.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import AutoComplete from "primevue/autocomplete";
 import Button from "primevue/button";
 import Checkbox from "primevue/checkbox";
 import Column from "primevue/column";
@@ -9,12 +10,13 @@ import DataTable from "primevue/datatable";
 import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
 import Message from "primevue/message";
+import Password from "primevue/password";
 import Toast from "primevue/toast";
 import ToastService from "primevue/toastservice";
-import Password from "primevue/password";
 
 Vue.use(ToastService);
 
+Vue.component("AutoComplete", AutoComplete);
 Vue.component("Button", Button);
 Vue.component("Checkbox", Checkbox);
 Vue.component("Column", Column);
@@ -22,8 +24,8 @@ Vue.component("DataTable", DataTable);
 Vue.component("Dialog", Dialog);
 Vue.component("InputText", InputText);
 Vue.component("Message", Message);
-Vue.component("Toast", Toast);
 Vue.component("Password", Password);
+Vue.component("Toast", Toast);
 
 import "primevue/resources/themes/nova/theme.css";
 import "primevue/resources/primevue.min.css";
