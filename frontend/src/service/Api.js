d6445c77cc02bff1c34fbf1504bbddc02946e096
Simen Røkaas <simen@simenro.net>
2020-12-25 22:01:38 +0100

    Fiksa api-url til å stemme overens med .httpaccess på server.

--- frontend/src/service/Api.js
+++ frontend/src/service/Api.js
@@ -4,7 +4,7 @@ export default () => {
   const apiUrl =
     process.env.VUE_APP_LOCAL_SERVER === "true"
       ? "http://localhost:57462"
-      : "https://www.noter.tjk.no";
+      : "https://noter.tjk.no";
 
   return axios.create({
     baseURL: apiUrl,
