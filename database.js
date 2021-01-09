3ce0057f77c18f0137604eee3ca948bdff7053bc
Simen Røkaas <simen@simenro.net>
2021-01-09 23:10:18 +0100

    Lagt til database.js på nytt etter å ha fjernet alle historiske versjoner der hemmeligheter lå i klartekst.

---
+++ database.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  connectionLimit: 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB,
+});
+
+pool.getConnection(function (err, connection) {
+  if (err) throw err; // not connected!
+  console.log("Koblet til TJK database!");
+
+  // When done with the connection, release it.
+  connection.release();
+});
+
+module.exports = pool;
